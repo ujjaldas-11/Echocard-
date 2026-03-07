957eb4cad2adb0579fd67a5d6c6068ad72a17564
ujjaldas-11 <dasu82058@gmail.com>
2026-03-07 12:33:02 +0530

    fixed issue

--- echocard/settings.py
+++ echocard/settings.py
@@ -159,6 +159,12 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
